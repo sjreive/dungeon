b5e6567b72d61722539abc10883d91a4545d9a43
Sarah Reive <sarahjreive@gmail.com>
2017-11-05 18:31:28 -0500

    Finished Ch. 6 in Beginning Ruby, added loop for user interaction

--- dungeon.rb
+++ dungeon.rb
@@ -8,22 +8,73 @@ class Dungeon
     @rooms ={} #equive to @rooms= Hash.new, empty hash
   end
 
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms[reference]
+  end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
+
+  def go(direction)
+    puts "You go " + direction.to_s + "and come to a..."
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
+
   def add_room(reference, name, description, connections)
     @rooms[reference] = Room.new(reference, name, description, connections)
   end
 
 end
 
+class Player
+  attr_accessor :name, :location 
 
-Player= Struct.new(:name, :location) #Struct is a class has attributes & stores data. Parameters are optional.
+  def initialize(name)
+    @name = name
+  end
+end
 
-Room= Struct.new(:reference, :name, :description, :connections) #creates a Room class
+class Room
+  attr_accessor :reference, :name, :description, :connections
+
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+
+  def full_description
+    @name +"\nYou are in " + @description
+  end
+end
 
 
-me = Player.new("Sarah")
-my_dungeon= Dungeon.new(me)
-puts my_dungeon.player.name
+player = Player.new("Sarah")
+my_dungeon= Dungeon.new(player)
 
+
+#Adding rooms to the dungeon
 my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave})
 
-my_dungeon.add_room(:smallcave, "Small Cave", "a small cavernous cave", {:east => :largecave})
+my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave})
+
+#Start dungeon by placing the player in the large cave
+my_dungeon.start(:largecave)
+direction= ""
+until direction == "exit"
+puts "Which direction would you like to go? Options: north, south, east, west- or, type 'exit' if you want to quit the game"
+direction= gets.chomp.downcase.to_sym
+my_dungeon.go(direction)
+end
